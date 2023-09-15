2a012eaa6c995e7938c50df46c97acdc88d0751f
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-09-15 08:05:03 -0300

    fix: change import

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import CreateCabinForm from "./CreateCabinForm";
-import { useDeleteCabin } from "./usedeleteCabin";
 import { formatCurrency } from "../../utils/helpers";
 import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin";
@@ -8,6 +7,7 @@ import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import { useDeleteCabin } from "./useDeleteCabin";
 
 const Img = styled.img`
   display: block;
